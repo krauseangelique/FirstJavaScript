dd9c9ea1ec7cb67766088a3ed9bd508777dd12f3
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-13 15:41:17 +0200

    Object in JS

---
+++ scripts/objectScript.js
@@ -0,0 +1,21 @@
+// Declare variables
+const chat = {
+    name: "Lucky",
+    age: 4,
+    weight: "6",
+    color: "Tigré Gris"
+};
+
+
+
+// Developement
+
+
+
+// Display: Output
+console.log(chat);
+console.log(chat['name']);// ca fonctionne MAIS ne le faites pas car c'est la syntaxe pour les tableaux
+console.log(`La couleur du chat est`, chat['color']); // La couleur du chat est Tigré Gris
+console.log(chat.age);
+console.log(`Le nom du chat est ${chat.name}`);
+
