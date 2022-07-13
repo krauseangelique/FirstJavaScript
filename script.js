8d20225faa7284ca38b81750cf9ba025fd620582
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-13 11:18:29 +0200

    use of backtick

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // camelCase
 // Declare variables
-const firstname = "Lucas";
-const lastname = "Ielli";
-let counter = "29"; // string
-let counted = 29; // number
+const firstname     = "Lucas";
+const lastname      = "Ielli";
+const myFirstname   = "Angélique";
+const myLastname    = "KRAUSE";
 
-// typeof Number('1') === 'number'; // Number essaie de convertir l'argument en nombre
+let counter         = "29"; // string
+let counted         = 29; // number
+
+let age             = "29 ans";
+let myAge           = "56 ans";
+let job             = "coach chez Sirius !";
+let myJob           = "apprenante chez Sirius !";
+
+
+// typeof Number('1') === 'number'; // Number try to convert argument to number
+
 // Developement
 
-/* ! into the Console
-   ---------------
-    typeof arrayTest
-    'undefined'
-*/
+    /* ! into the Console
+    ---------------
+        typeof arrayTest
+        'undefined'
+    */
 
-// Display
+
+// Display: Output
 console.log(typeof counter); // string
 console.log(typeof counted); // number
+
+console.log(`Je m'appelle ${firstname}, j'ai ${age} et je suis ${job}`);
+console.log(`Je m'appelle ${myFirstname}, j'ai ${myAge} et je suis ${myJob}`);
 
