12e5526f12e488b509a4291a6ef51d1a4de23961
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-13 14:26:12 +0200

    favicon Ecmascript

--- scripts/tableScript.js
+++ scripts/tableScript.js
@@ -1,4 +1,21 @@
-let studentArray = [];
-studentArray = ['Arnaud','Angélique'];
+// Declare variables
+let theFirstname       = new String("Lucas"); 
+/*
+    String {'Lucas'}0: "L"1: "u"2: "c"3: "a"4: "s"length: 5[[Prototype]]: String[[PrimitiveValue]]: "Lucas"
 
+*/
+let arrayTest       = new Array();
+let studentArray    = [];
+studentArray        = ['Arnaud','Angélique'];
+let simpleString    = "Lucas"; // String
+let simpleNumber    = 0; // Number
+let simpleBoolean   = true; // Boolean 
+
+
+// Developement
+
+
+// Display: Output
+console.log(theFirstname);
 console.log(studentArray.length);
+console.log(studentArray[0]);
