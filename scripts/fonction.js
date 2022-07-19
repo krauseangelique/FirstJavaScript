38e0f1834f104d70af29954c120cc2643dc70b22
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-19 15:40:29 +0200

    Update fonction.js

--- scripts/fonction.js
+++ scripts/fonction.js
@@ -35,18 +35,62 @@ document.getElementById("btnLinkTop");
 
 // fonction 3 param
 function generateRandom() {
-    const r1 = Math.floor(Math.random() * 256);
-    const g1 = Math.floor(Math.random() * 256);
-    const b1 = Math.floor(Math.random() * 256);
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
 
-    document.body.style.background = `rgb(${r1}, ${g1}, ${b1})`;
+    document.body.style.background = `rgb(${red}, ${green}, ${blue})`;
 }
 
 // Call to function
 // generateRandom();
 document.getElementById("randomBg").addEventListener("click", generateRandom);
 
+// callback function
+let siriusTeamArray = ["Jeremy", "Ganaëlle", "Lucas"];
 
+siriusTeamArray.forEach((element) => {
+
+    console.log(element);
+    
+})
+
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augustus', 'September', 'October', 'November', 'December'];
+
+let randomNumber = Math.floor(Math.random() * months.length);
+console.log(months[randomNumber]);
+
+var x = parseInt(prompt("Enter a Value", "0"), 10);
+var y = parseInt(prompt("Enter a Value", "0"), 10);
+
+var x = parseFloat(prompt("Enter a Value", "0"));
+var y = parseFloat(prompt("Enter a Value", "0"));
+
+function circumference(r) {
+    if (Number.isNaN(Number.parseFloat(r))) {
+      return 0;
+    }
+    return parseFloat(r) * 2.0 * Math.PI ;
+  }
+  
+  console.log(circumference('4.567abcdefgh'));
+  // expected output: 28.695307297889173
+  
+  console.log(circumference('abcdefgh'));
+  // expected output: 0
+  let string = "2.356";
+  let money = parseFloat(string);
+  console.log(money);
+/*Copy to Clipboard
+Paramètres
+string
+Une chaîne de caractères la valeur qu'on souhaite analyser et transformer en un nombre flottant.
+
+Valeur de retour
+Un nombre flottant obtenu à partir de l'analyse de la chaîne de caractères. Si le premier caractère ne permet pas d'obtenir un nombre, ce sera la valeur NaN qui sera renvoyée.
+
+Description
+*/
 
 
    
