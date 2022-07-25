d0d89f7234e55687af0ccbc68f55998e4ce5bee4
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-25 11:17:51 +0200

    methode slice, splice and findIndex

--- scripts/methodes.js
+++ scripts/methodes.js
@@ -2,11 +2,70 @@
  *  METHODES EN JS
  */
 
- let fruits = ["Pomme", "Orange", "Fraise"];
 /******
+ * slice : copie SUPERFICIELLE d'une portion du tableau d'origine, portion définie par 
+ * un indice de début et un indice de fin (exclus) [ ,  [
+ *  ! le tableau d'origine n'est pas modifié
+ */ 
+ let fruits     = ["Pomme", "Orange", "Fraise"];
+ let copyFruits = fruits.slice();
+ console.log(copyFruits);
+ 
+
+ const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+ console.log(animals.slice(2)); // expected output (3) ['camel', 'duck', 'elephant']
+
+/****
+ * splice : permet de MODIFIER les éléments d'un tableau
+ */
+ var mesPoissons  = ["scalaire", "clown", "mandarin", "chirurgien"];
+
+ // supprime 0 élément à partir de l'index 2, et insère "tambour"
+ var enleves = mesPoissons.splice(2, 0, "tambour");
+ // mesPoissons est ["scalaire", "clown", "tambour", "mandarin", "chirurgien"]
+ // enleves est [], aucun élément supprimé
+ 
+ // supprime 1 élément à partir de l'index 3
+ enleves = mesPoissons.splice(3, 1);
+ // mesPoissons est ["scalaire", "clown", "tambour", "chirurgien"]
+ // enleves est ["mandarin"]
+ 
+ // supprime 1 élément à partir de l'index 2, et insère "trompette"
+ enleves = mesPoissons.splice(2, 1, "trompette");
+ // mesPoissons est ["scalaire", "clown", "trompette", "chirurgien"]
+ // enleves est ["tambour"]
+ 
+ // supprime 2 éléments à partir de l'index 0, et insère "perroquet", "anémone" et"bleu"
+ enleves = mesPoissons.splice(0, 2, "perroquet", "anémone", "bleu");
+ // mesPoissons est ["perroquet", "anémone", "bleu", "trompette", "chirurgien"]
+ // enleves est ["scalaire", "clown"]
+ 
+ // supprime 2 éléments à partir de l'indice 2
+ enleves = mesPoissons.splice(mesPoissons.length - 3, 2);
+ // mesPoissons est ["perroquet", "anémone", "chirurgien"]
+ // enleves est ["bleu", "trompette"]
+ 
+ var mesPoissons = ["perroquet", "anémone", "bleu", "trompette", "chirurgien"];
+ // on retire trois éléments à partir de l'indice 2
+ enleves = mesPoissons.splice(2);
+ // mesPoissons vaut ["perroquet", "anémone"]
+ // enleves vaut ["bleu", "trompette", "chirurgien"]
+ 
+ var mesAnimaux = ["cheval", "chien", "chat", "dauphin"];
+ enleves = mesAnimaux.splice(-2, 1);
+ 
+ // mesAnimaux vaut ["cheval", "chien", "dauphin"]
+ // enleves vaut ["chat"]
+
+
+/* ****
  * findIndex
  */
-
+let mesfruits     = ["Pomme", "Orange", "Fraise"];
+// méthode findIndex qui permet de trouver une correspondance entre un elmt et son index dans le tableau
+const index = mesfruits.findIndex(element => element === 'Pomme');
+console.log(index); // 0
+console.log(mesfruits[index]); // Pomme
 
 /******
  * filter
